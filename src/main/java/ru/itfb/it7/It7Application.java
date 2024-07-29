1b7cac94b025b158c93c8b112f3cab2a35444de5
artur <arturgilman@mail.ru>
2024-07-29 09:44:54 +0500

    new proj, migrations

---
+++ src/main/java/ru/itfb/it7/It7Application.java
@@ -0,0 +1,13 @@
+package ru.itfb.it7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It7Application.class, args);
+	}
+
+}
